35d0d7a359cdb549e28bb9cf34c8ef17e1e17452
zharkowski <maxim199919@rambler.ru>
2020-07-05 03:12:54 +0300

    подключен роутер

--- src/compoents/app/app.jsx
+++ src/compoents/app/app.jsx
@@ -1,18 +1,29 @@
 import React from "react";
 import PropTypes from "prop-types";
+import {BrowserRouter, Switch, Route} from "react-router-dom";
 import Main from "../main/main.jsx";
+import FilmPage from "../film-page/film-page.jsx";
 
 const App = (props) => {
   const {title, genre, releaseYear, films} = props;
   const onFilmTitleClick = () => {};
   return (
-    <Main
-      title={title}
-      genre={genre}
-      releaseYear={releaseYear}
-      films={films}
-      onFilmTitleClick={onFilmTitleClick}
-    />
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/">
+          <Main
+            title={title}
+            genre={genre}
+            releaseYear={releaseYear}
+            films={films}
+            onFilmTitleClick={onFilmTitleClick}
+          />
+        </Route>
+        <Route exact path="/dev-film-page">
+          <FilmPage/>
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 };
 
