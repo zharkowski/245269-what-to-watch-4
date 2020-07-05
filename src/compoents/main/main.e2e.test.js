5e20d58b773fd9c1b7169607291fbe9a99b7d276
zharkowski <maxim199919@rambler.ru>
2020-07-05 17:51:26 +0300

    обновлены e2e тесты для обработчки клика по заголовку и изображению фильма

--- src/compoents/main/main.e2e.test.js
+++ src/compoents/main/main.e2e.test.js
@@ -1,11 +1,37 @@
 import React from "react";
-import Enzyme, {shallow} from "enzyme";
+import Enzyme, {shallow, mount} from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
 import Main from "./main.jsx";
 
 Enzyme.configure({
   adapter: new Adapter(),
 });
+
+const films = [{
+  title: `Fantastic Beasts: The Crimes of Grindelwald`,
+  picture: `img/fantastic-beasts-the-crimes-of-grindelwald.jpg`,
+}, {
+  title: `Bohemian Rhapsody`,
+  picture: `img/bohemian-rhapsody.jpg`,
+}, {
+  title: `Macbeth`,
+  picture: `img/macbeth.jpg`,
+}, {
+  title: `Aviator`,
+  picture: `img/aviator.jpg`,
+}, {
+  title: `We need to talk about Kevin`,
+  picture: `img/we-need-to-talk-about-kevin.jpg`,
+}, {
+  title: `What We Do in the Shadows`,
+  picture: `img/what-we-do-in-the-shadows.jpg`,
+}, {
+  title: `Revenant`,
+  picture: `img/revenant.jpg`,
+}, {
+  title: `Shutter Island`,
+  picture: `img/shutter-island.jpg`,
+}];
 
 const mockEvent = {
   preventDefault() {}
@@ -14,33 +40,7 @@ const mockEvent = {
 describe(`Main e2e test`, () => {
   it(`Should film title be pressed`, () => {
     const onFilmClick = jest.fn();
-    const films = [{
-      title: `Fantastic Beasts: The Crimes of Grindelwald`,
-      picture: `img/fantastic-beasts-the-crimes-of-grindelwald.jpg`,
-    }, {
-      title: `Bohemian Rhapsody`,
-      picture: `img/bohemian-rhapsody.jpg`,
-    }, {
-      title: `Macbeth`,
-      picture: `img/macbeth.jpg`,
-    }, {
-      title: `Aviator`,
-      picture: `img/aviator.jpg`,
-    }, {
-      title: `We need to talk about Kevin`,
-      picture: `img/we-need-to-talk-about-kevin.jpg`,
-    }, {
-      title: `What We Do in the Shadows`,
-      picture: `img/what-we-do-in-the-shadows.jpg`,
-    }, {
-      title: `Revenant`,
-      picture: `img/revenant.jpg`,
-    }, {
-      title: `Shutter Island`,
-      picture: `img/shutter-island.jpg`,
-    }];
-
-    const main = shallow(
+    const main = mount(
         <Main
           title={`The Grand Budapest Hotel`}
           genre={`Drama`}
@@ -49,9 +49,26 @@ describe(`Main e2e test`, () => {
           onFilmClick={onFilmClick}/>
     );
 
-    const filmCards = main.find(`FilmsList`).dive().find(`FilmCard`);
-    const filmTitles = filmCards.map((filmCard) => filmCard.dive().find(`.small-movie-card__title`));
+    const filmCards = main.find(`FilmCard`);
+    const filmTitles = filmCards.map((filmCard) => filmCard.find(`.small-movie-card__title`));
     filmTitles.map((title) => title.simulate(`click`, mockEvent));
+    expect(onFilmClick.mock.calls.length).toBe(films.length);
+  });
+
+  it(`Should film image be pressed`, () => {
+    const onFilmClick = jest.fn();
+    const main = mount(
+        <Main
+          title={`The Grand Budapest Hotel`}
+          genre={`Drama`}
+          releaseYear={2014}
+          films={films}
+          onFilmClick={onFilmClick}/>
+    );
+
+    const filmCards = main.find(`FilmCard`);
+    const filmImages = filmCards.map((filmCard) => filmCard.find(`.small-movie-card__image`));
+    filmImages.map((image) => image.simulate(`click`, mockEvent));
     expect(onFilmClick.mock.calls.length).toBe(films.length);
   });
 });
