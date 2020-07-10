70c14116db9b56b530815048ac0751aa2d81672e
zharkowski <maxim199919@rambler.ru>
2020-07-10 15:29:41 +0300

    в моки добавлены ссылки на видео

--- src/mocks/films.js
+++ src/mocks/films.js
@@ -1,27 +1,35 @@
 export const films = [{
   title: `Fantastic Beasts: The Crimes of Grindelwald`,
   picture: `img/fantastic-beasts-the-crimes-of-grindelwald.jpg`,
+  src: `https://download.blender.org/durian/trailer/sintel_trailer-480p.mp4`,
 }, {
   title: `Bohemian Rhapsody`,
   picture: `img/bohemian-rhapsody.jpg`,
+  src: `https://upload.wikimedia.org/wikipedia/commons/transcoded/b/b3/Big_Buck_Bunny_Trailer_400p.ogv/Big_Buck_Bunny_Trailer_400p.ogv.360p.webm`,
 }, {
   title: `Macbeth`,
   picture: `img/macbeth.jpg`,
+  src: `https://download.blender.org/durian/trailer/sintel_trailer-480p.mp4`,
 }, {
   title: `Aviator`,
   picture: `img/aviator.jpg`,
+  src: `https://upload.wikimedia.org/wikipedia/commons/transcoded/b/b3/Big_Buck_Bunny_Trailer_400p.ogv/Big_Buck_Bunny_Trailer_400p.ogv.360p.webm`,
 }, {
   title: `We need to talk about Kevin`,
   picture: `img/we-need-to-talk-about-kevin.jpg`,
+  src: `https://download.blender.org/durian/trailer/sintel_trailer-480p.mp4`,
 }, {
   title: `What We Do in the Shadows`,
   picture: `img/what-we-do-in-the-shadows.jpg`,
+  src: `https://upload.wikimedia.org/wikipedia/commons/transcoded/b/b3/Big_Buck_Bunny_Trailer_400p.ogv/Big_Buck_Bunny_Trailer_400p.ogv.360p.webm`,
 }, {
   title: `Revenant`,
   picture: `img/revenant.jpg`,
+  src: `https://download.blender.org/durian/trailer/sintel_trailer-480p.mp4`,
 }, {
   title: `Shutter Island`,
   picture: `img/shutter-island.jpg`,
+  src: `https://upload.wikimedia.org/wikipedia/commons/transcoded/b/b3/Big_Buck_Bunny_Trailer_400p.ogv/Big_Buck_Bunny_Trailer_400p.ogv.360p.webm`,
 }];
 
 export const filmDetails = {
